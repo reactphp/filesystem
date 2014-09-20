93b6558f1d46b1cce217d45dfed813f0f08878b6
Cees-Jan Kiewiet <ceesjank@gmail.com>
2014-09-20 18:31:49 +0200

    File object now also supports creating, opening and closing

--- src/File.php
+++ src/File.php
@@ -3,43 +3,91 @@
 namespace React\Filesystem;
 
 use React\Filesystem\Filesystem\FilesystemInterface;
+use React\Promise\Deferred;
 
-class File {
+class File
+{
 
-    use SharedTrait;
+    use GenericNodeOperationTrait;
 
-    public function __construct($filename, FilesystemInterface $filesystem) {
+    protected $open = false;
+    protected $fileDescriptor;
+
+    public function __construct($filename, FilesystemInterface $filesystem)
+    {
         $this->filename = $filename;
         $this->filesystem = $filesystem;
     }
 
-    protected function getPath() {
+    protected function getPath()
+    {
         return $this->filename;
     }
 
-    public function exists() {
-        return $this->filesystem->stat($this->filename)->then(function($result) {
+    public function exists()
+    {
+        return $this->filesystem->stat($this->filename)->then(function ($result) {
             return $result;
         });
     }
 
-    public function size() {
+    public function size()
+    {
         return $this->filesystem->stat($this->filename)->then(function ($result) {
             return $result['size'];
         });
     }
 
-    public function time() {
+    public function time()
+    {
         return $this->filesystem->stat($this->filename)->then(function ($result) {
             return [
-                'atime' => $result['atime'],
-                'ctime' => $result['ctime'],
-                'mtime' => $result['mtime'],
+            'atime' => $result['atime'],
+            'ctime' => $result['ctime'],
+            'mtime' => $result['mtime'],
             ];
         });
     }
 
-    public function remove() {
+    public function create()
+    {
+        $deferred = new Deferred();
+
+        $this->stat()->then(null, function () use ($deferred) {
+            $deferred->reject(new EioException('File exists'));
+        })->then(function () {
+            return $this->filesystem->touch($this->filename);
+        });
+
+        return $deferred->promise();
+    }
+
+    public function open($flags)
+    {
+        $deferred = new Deferred();
+
+        return $this->filesystem->open($this->filename, $flags)->then(function ($fd) use ($deferred) {
+            $this->open = true;
+            $this->fileDescriptor = $fd;
+
+            return $deferred->promise();
+        });
+    }
+
+    public function close()
+    {
+        $deferred = new Deferred();
+
+        return $this->filesystem->close($this->fileDescriptor)->then(function () use ($deferred) {
+            $this->open = false;
+            $this->fileDescriptor = null;
+
+            return $deferred->promise();
+        });
+    }
+
+    public function remove()
+    {
         return $this->filesystem->unlink($this->filename);
     }
 }
