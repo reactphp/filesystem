9ccd43b2242614fb3abb5d544ee6e99ac6112881
Cees-Jan Kiewiet <ceesjank@gmail.com>
2014-09-27 14:03:57 +0200

    Eio test getLoop

--- tests/EioFilesystemTest.php
+++ tests/EioFilesystemTest.php
@@ -4,14 +4,26 @@ namespace React\Tests\Filesystem;
 
 use React\Filesystem\EioFilesystem;
 
-class EioFilesystemTest extends \PHPUnit_Framework_TestCase {
+class EioFilesystemTest extends \PHPUnit_Framework_TestCase
+{
 
-    public function testEioExtensionInstalled() {
+    public function testEioExtensionInstalled()
+    {
         $this->assertTrue(function_exists('eio_init'));
     }
 
-    public function testInterface() {
-        $this->assertInstanceOf('React\Filesystem\FilesystemInterface', new EioFilesystem($this->getMock('React\EventLoop\LoopInterface')));
+    public function testInterface()
+    {
+        $this->assertInstanceOf(
+            'React\Filesystem\FilesystemInterface',
+            new EioFilesystem($this->getMock('React\EventLoop\LoopInterface'))
+        );
     }
 
+    public function testGetLoop()
+    {
+        $loop = $this->getMock('React\EventLoop\LoopInterface');
+        $filesystem = new EioFilesystem($loop);
+        $this->assertSame($loop, $filesystem->getLoop());
+    }
 }
