60f7f50c019b89239d97d81057ca21a424b12286
Cees-Jan Kiewiet <ceesjank@gmail.com>
2014-10-04 23:57:37 +0200

    Basic ls success test

--- tests/Node/DirectoryTest.php
+++ tests/Node/DirectoryTest.php
@@ -18,9 +18,7 @@ class DirectoryTest extends \PHPUnit_Framework_TestCase
             $loop,
         ]);
 
-        $lsPromise = $this->getMock('React\Promise\PromiseInterface', [
-            'then',
-        ]);
+        $lsPromise = $this->getMock('React\Promise\PromiseInterface');
 
 
         $filesystem
@@ -28,6 +26,72 @@ class DirectoryTest extends \PHPUnit_Framework_TestCase
             ->method('ls')
             ->with($path)
             ->will($this->returnValue($lsPromise))
+        ;
+
+        $directory = new Directory($path, $filesystem);
+        $this->assertInstanceOf('React\Promise\PromiseInterface', $directory->ls());
+    }
+
+    public function testLsSuccess()
+    {
+        $dents = [
+            'dents' => [
+                [
+                    'type' => EIO_DT_DIR,
+                    'name' => 'bar',
+                ],
+                [
+                    'type' => EIO_DT_REG,
+                    'name' => 'foo',
+                ],
+            ],
+        ];
+        $path = '/home/foo/bar';
+        $loop = $this->getMock('React\EventLoop\StreamSelectLoop', [
+            'futureTick',
+        ]);
+
+        $loop
+            ->expects($this->once())
+            ->method('futureTick')
+            ->with($this->isType('callable'))
+            ->will($this->returnCallback(function($callback) use ($dents) {
+                $callback($dents);
+            }))
+        ;
+
+        $filesystem = $this->getMock('React\Filesystem\EioFilesystem', [
+            'ls',
+            'getLoop',
+        ], [
+            $loop,
+        ]);
+
+        $lsPromise = $this->getMock('React\Promise\PromiseInterface', [
+            'then',
+        ]);
+
+        $filesystem
+            ->expects($this->once())
+            ->method('ls')
+            ->with($path)
+            ->will($this->returnValue($lsPromise))
+        ;
+
+        $filesystem
+            ->expects($this->once())
+            ->method('getLoop')
+            ->with()
+            ->will($this->returnValue($loop))
+        ;
+
+        $lsPromise
+            ->expects($this->once())
+            ->method('then')
+            ->with($this->isType('callable'), $this->isType('callable'))
+            ->will($this->returnCallback(function($resolveCb) use ($dents) {
+                $resolveCb($dents);
+            }))
         ;
 
         $directory = new Directory($path, $filesystem);
