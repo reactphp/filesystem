a784b339301148c9d595ae16018da325c9221f86
Cees-Jan Kiewiet <ceesjank@gmail.com>
2014-12-09 06:36:10 +0100

    WritableStream::close tests

--- tests/Stream/WritableStreamTest.php
+++ tests/Stream/WritableStreamTest.php
@@ -122,4 +122,90 @@ class WritableStreamTest extends \PHPUnit_Framework_TestCase
 
         $stream->end();
     }
+
+    public function testClose()
+    {
+        $path = 'foo.bar';
+        $fd = '0123456789abcdef';
+
+        $filesystem = $this->getMock('React\Filesystem\EioAdapter', [
+            'close',
+        ], [
+            $this->getMock('React\EventLoop\StreamSelectLoop'),
+        ]);
+
+
+        $promise = $this->getMock('React\Promise\PromiseInterface', [
+            'then',
+        ]);
+
+        $promise
+            ->expects($this->once())
+            ->method('then')
+            ->with($this->isType('callable'))
+            ->will($this->returnCallback(function ($resolveCb) {
+                $resolveCb();
+            }))
+        ;
+
+        $filesystem
+            ->expects($this->once())
+            ->method('close')
+            ->with($fd)
+            ->will($this->returnValue($promise))
+        ;
+
+        $stream = $this->getMock('React\Filesystem\Eio\WritableStream', [
+            'emit',
+            'removeAllListeners',
+        ], [
+            $path,
+            $fd,
+            $filesystem,
+        ]);
+
+        $stream
+            ->expects($this->at(0))
+            ->method('emit')
+            ->with('end', [$stream])
+        ;
+
+        $stream
+            ->expects($this->at(1))
+            ->method('emit')
+            ->with('close', [$stream])
+        ;
+
+        $stream
+            ->expects($this->at(2))
+            ->method('removeAllListeners')
+            ->with()
+        ;
+
+        $stream->close();
+    }
+
+    public function testAlreadyClosed()
+    {
+        $path = 'foo.bar';
+        $fd = '0123456789abcdef';
+        $filesystem = $this->getMock('React\Filesystem\EioAdapter', [
+            'close',
+        ], [
+            $this->getMock('React\EventLoop\StreamSelectLoop'),
+        ]);
+
+
+        $filesystem
+            ->expects($this->once())
+            ->method('close')
+            ->with($fd)
+            ->will($this->returnValue(new RejectedPromise()))
+        ;
+
+
+        $stream = (new WritableStream($path, $fd, $filesystem));
+        $stream->close();
+        $stream->close();
+    }
 }
