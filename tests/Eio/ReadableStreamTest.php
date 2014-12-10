4eada0ebd4dcd55f17d961886044458a92d478bb
Cees-Jan Kiewiet <ceesjank@gmail.com>
2014-12-10 09:46:08 +0100

    Made a start with ReadableStream tests

---
+++ tests/Eio/ReadableStreamTest.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace React\Tests\Filesystem\Stream;
+
+class ReadableStreamTest extends \PHPUnit_Framework_TestCase
+{
+    public function testConstruct()
+    {
+        $path = 'foo.bar';
+        $fd = '0123456789abcdef';
+
+        $promise = $this->getMock('React\Promise\PromiseInterface', [
+            'then',
+        ]);
+
+        $promise
+            ->expects($this->once())
+            ->method('then')
+            ->with($this->isType('callable'))
+            ->will($this->returnCallback(function ($resolveCb) {
+                $resolveCb([
+                    'size' => 123,
+                ]);
+            }))
+        ;
+
+        $filesystem = $this->getMock('React\Filesystem\EioAdapter', [], [
+            $this->getMock('React\EventLoop\StreamSelectLoop'),
+        ]);
+
+        $filesystem
+            ->expects($this->once())
+            ->method('stat')
+            ->with($path)
+            ->will($this->returnValue($promise))
+        ;
+
+        $this->getMock('React\Filesystem\Eio\ReadableStream', [
+            'readChunk',
+        ], [
+            $path,
+            $fd,
+            $filesystem,
+        ]);
+    }
+
+    public function testResume()
+    {
+        $path = 'foo.bar';
+        $fd = '0123456789abcdef';
+
+        $promise = $this->getMock('React\Promise\PromiseInterface', [
+            'then',
+        ]);
+
+        $promise
+            ->expects($this->once())
+            ->method('then')
+            ->with($this->isType('callable'))
+            ->will($this->returnCallback(function ($resolveCb) {
+                $resolveCb([
+                    'size' => 123,
+                ]);
+            }))
+        ;
+
+        $filesystem = $this->getMock('React\Filesystem\EioAdapter', [], [
+            $this->getMock('React\EventLoop\StreamSelectLoop'),
+        ]);
+
+        $filesystem
+            ->expects($this->once())
+            ->method('stat')
+            ->with($path)
+            ->will($this->returnValue($promise))
+        ;
+
+        $stream = $this->getMock('React\Filesystem\Eio\ReadableStream', [
+            'readChunk',
+        ], [
+            $path,
+            $fd,
+            $filesystem,
+        ]);
+    }
+}
