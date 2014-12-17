c33e80d08301ecc0d1d5fe848deb5a9dd16e7488
Cees-Jan Kiewiet <ceesjank@gmail.com>
2014-12-17 08:35:01 +0100

    EioAdapter tests for unlink, rename, chmod, chown, ls, mkdir, and rmdir

--- tests/EioAdapterTest.php
+++ tests/EioAdapterTest.php
@@ -2,6 +2,7 @@
 
 namespace React\Tests\Filesystem;
 
+use React\Filesystem\Eio\PermissionFlagResolver;
 use React\Filesystem\EioAdapter;
 
 class EioFilesystemTest extends \PHPUnit_Framework_TestCase
@@ -25,5 +26,142 @@ class EioFilesystemTest extends \PHPUnit_Framework_TestCase
         $loop = $this->getMock('React\EventLoop\LoopInterface');
         $filesystem = new EioAdapter($loop);
         $this->assertSame($loop, $filesystem->getLoop());
+    }
+
+    public function testCallEioCallsProvider()
+    {
+        $pathName = 'foo.bar';
+        return [
+            [
+                'unlink',
+                'eio_unlink',
+                [
+                    $pathName,
+                ],
+                [
+                    $pathName,
+                ],
+            ],
+            [
+                'rename',
+                'eio_rename',
+                [
+                    $pathName,
+                    str_rot13($pathName),
+                ],
+                [
+                    $pathName,
+                    str_rot13($pathName),
+                ],
+            ],
+            [
+                'chmod',
+                'eio_chmod',
+                [
+                    $pathName,
+                    123,
+                ],
+                [
+                    $pathName,
+                    123,
+                ],
+            ],
+            [
+                'chown',
+                'eio_chown',
+                [
+                    $pathName,
+                    1,
+                    2,
+                ],
+                [
+                    $pathName,
+                    1,
+                    2,
+                ],
+            ],
+            [
+                'ls',
+                'eio_readdir',
+                [
+                    $pathName,
+                ],
+                [
+                    $pathName,
+                    EIO_READDIR_DIRS_FIRST,
+                ],
+                false,
+            ],
+            [
+                'ls',
+                'eio_readdir',
+                [
+                    $pathName,
+                    112,
+                ],
+                [
+                    $pathName,
+                    112,
+                ],
+                false,
+            ],
+            [
+                'mkdir',
+                'eio_mkdir',
+                [
+                    $pathName,
+                ],
+                [
+                    $pathName,
+                    (new PermissionFlagResolver())->resolve(EioAdapter::CREATION_MODE),
+                ],
+            ],
+            [
+                'mkdir',
+                'eio_mkdir',
+                [
+                    $pathName,
+                    'rwxrwxrwx',
+                ],
+                [
+                    $pathName,
+                    (new PermissionFlagResolver())->resolve('rwxrwxrwx'),
+                ],
+            ],
+            [
+                'rmdir',
+                'eio_rmdir',
+                [
+                    $pathName,
+                ],
+                [
+                    $pathName,
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider testCallEioCallsProvider
+     */
+    public function testCallEioCalls($externalMethod, $internalMethod, $externalCallArgs, $internalCallArgs, $errorResultCode = -1)
+    {
+        $promise = $this->getMock('React\Promise\PromiseInterface');
+
+        $filesystem = $this->getMock('React\Filesystem\EioAdapter', [
+            'callEio',
+        ], [
+            $this->getMock('React\EventLoop\LoopInterface'),
+        ]);
+
+        $filesystem
+            ->expects($this->once())
+            ->method('callEio')
+            ->with($internalMethod, $internalCallArgs, $errorResultCode)
+            ->will($this->returnValue($promise))
+        ;
+
+        $this->assertSame($promise, call_user_func_array([$filesystem, $externalMethod], $externalCallArgs));
+
     }
 }
