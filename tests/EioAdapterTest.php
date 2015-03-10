fb4cd0f0527ecd75267899f30f8b2afa8c06f7b5
Cees-Jan Kiewiet <ceesjank@gmail.com>
2015-03-10 21:52:48 +0100

    Classname is now match filename

--- tests/EioAdapterTest.php
+++ tests/EioAdapterTest.php
@@ -6,7 +6,7 @@ use React\Filesystem\Eio\PermissionFlagResolver;
 use React\Filesystem\EioAdapter;
 use React\Promise\FulfilledPromise;
 
-class EioFilesystemTest extends \PHPUnit_Framework_TestCase
+class EioAdapterTest extends \PHPUnit_Framework_TestCase
 {
 
     public function testEioExtensionInstalled()
