3ab9c248db441c5630f369aee5df2ede052b8c00
Cees-Jan Kiewiet <ceesjank@gmail.com>
2015-03-22 14:48:31 +0100

    Throttled queued invoker to run evens in a queue with a certain interval

---
+++ src/ThrottledQueuedInvoker.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace React\Filesystem;
+
+use React\EventLoop\LoopInterface;
+use React\Promise\Deferred;
+
+class ThrottledQueuedInvoker implements CallInvokerInterface
+{
+    /**
+     * @var LoopInterface
+     */
+    protected $loop;
+
+    /**
+     * @var AdapterInterface
+     */
+    protected $adapter;
+
+    /**
+     * @var \SplQueue
+     */
+    protected $callQueue;
+
+    /**
+     * @var bool
+     */
+    protected $callQueueActive = false;
+
+    /**
+     * @var float
+     */
+    protected $interval = 2.3;
+
+    /**
+     * @param AdapterInterface $adapter
+     */
+    public function __construct(AdapterInterface $adapter)
+    {
+        $this->loop = $adapter->getLoop();
+        $this->adapter = $adapter;
+        $this->callQueue = new \SplQueue();
+    }
+
+    /**
+     * @return float
+     */
+    public function getInterval()
+    {
+        return $this->interval;
+    }
+
+    /**
+     * @param float $interval
+     */
+    public function setInterval($interval)
+    {
+        $this->interval = $interval;
+    }
+
+    /**
+     * @param string $function
+     * @param array $args
+     * @param int $errorResultCode
+     * @return \React\Promise\PromiseInterface
+     */
+    public function invokeCall($function, $args, $errorResultCode = -1)
+    {
+        $this->callQueueActive = true;
+        $deferred = new Deferred();
+
+        $this->callQueue->enqueue(new QueuedCall($deferred, $function, $args, $errorResultCode));
+
+        if (!$this->callQueue->isEmpty()) {
+            $this->processQueue();
+        }
+
+        return $deferred->promise()->then(function ($data) {
+            return $this->
+                adapter->
+                callFilesystem($data['function'], $data['args'], $data['errorResultCode'])->
+                then($this->filesystemResultHandler('React\Promise\resolve'), $this->filesystemResultHandler('React\Promise\reject'));
+        });
+    }
+
+    protected function processQueue()
+    {
+        $this->loop->addTimer($this->interval, function () {
+            if ($this->callQueue->isEmpty()) {
+                return;
+            }
+
+            $message = $this->callQueue->dequeue();
+            $data = [
+                'function' => $message->getFunction(),
+                'args' => $message->getArgs(),
+                'errorResultCode' => $message->getErrorResultCode(),
+            ];
+            $message->getDeferred()->resolve($data);
+        });
+    }
+
+    protected function filesystemResultHandler($func)
+    {
+        return function ($mixed) use ($func) {
+            if ($this->callQueue->count() == 0) {
+                $this->callQueueActive = false;
+            } else {
+                $this->processQueue();
+            }
+            return $func($mixed);
+        };
+    }
+}
---
+++ tests/ThrottledQueuedInvokerTest.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace React\Tests\Filesystem;
+
+use React\EventLoop\Factory;
+use React\Filesystem\ThrottledQueuedInvoker;
+use React\Promise\FulfilledPromise;
+
+class ThrottledQueuedInvokerTest extends \PHPUnit_Framework_TestCase
+{
+    public function testInvokeCall()
+    {
+        $loop = Factory::create();
+
+        $function = [
+            'foo',
+            'bar',
+            'baz',
+        ];
+        $args = [
+            [
+                'bar',
+                'baz',
+            ],
+            [
+                'baz',
+                'foo',
+            ],
+            [
+                'foo',
+                'bar',
+            ],
+        ];
+        $errorResultCode = [
+            13,
+            14,
+            42,
+        ];
+
+        $filesystem = $this->getMock('React\Filesystem\EioAdapter', [
+            'callFilesystem',
+        ], [
+            $loop,
+        ]);
+
+        foreach ($function as $key => $value) {
+            $filesystem
+                ->expects($this->at($key))
+                ->method('callFilesystem')
+                ->with($function[$key], $args[$key], $errorResultCode[$key])
+                ->will($this->returnValue(new FulfilledPromise()))
+            ;
+        }
+
+        $invoker = new ThrottledQueuedInvoker($filesystem);
+        foreach ($function as $key => $value) {
+            $invoker->invokeCall($function[$key], $args[$key], $errorResultCode[$key]);
+        }
+
+        $loop->run();
+    }
+
+    public function testInterval()
+    {
+        $invoker = new ThrottledQueuedInvoker($this->getMock('React\Filesystem\EioAdapter', [], [
+            Factory::create(),
+        ]));
+        $this->assertSame(2.3, $invoker->getInterval());
+        $invoker->setInterval(3.2);
+        $this->assertSame(3.2, $invoker->getInterval());
+    }
+}
