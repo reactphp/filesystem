294d5cea1983abb5cd0237438ce14aa42d1e05cc
Cees-Jan Kiewiet <ceesjank@gmail.com>
2015-03-28 22:22:22 +0100

    Two typo's

--- tests/CallInvokerProvider.php
+++ tests/CallInvokerProvider.php
@@ -31,7 +31,7 @@ class CallInvokerProvider extends \PHPUnit_Framework_TestCase
         $invokers['instant'][1] = $this->getMock('React\Filesystem\EioAdapter', $mockedMethods, [
             $invokers['instant'][0],
         ]);
-        $invokers['instant'][2] = new InstantInvoker($invokers['pooled'][1]);
+        $invokers['instant'][2] = new InstantInvoker($invokers['instant'][1]);
         $invokers['instant'][1]->setInvoker($invokers['instant'][2]);
 
         $invokers['queued'][0] = Factory::create();
@@ -45,7 +45,7 @@ class CallInvokerProvider extends \PHPUnit_Framework_TestCase
         $invokers['throttledqueued'][1] = $this->getMock('React\Filesystem\EioAdapter', $mockedMethods, [
             $invokers['throttledqueued'][0],
         ]);
-        $invokers['throttledqueued'][2] = new ThrottledQueuedInvoker($invokers['pooled'][1]);
+        $invokers['throttledqueued'][2] = new ThrottledQueuedInvoker($invokers['throttledqueued'][1]);
         $invokers['throttledqueued'][1]->setInvoker($invokers['throttledqueued'][2]);
 
         return $invokers;
