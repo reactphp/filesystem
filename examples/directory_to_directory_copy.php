be1bf4f5530595760264de3fb2e9cb4c9a9cbec5
Cees-Jan Kiewiet <ceesjank@gmail.com>
2015-07-03 21:44:38 +0200

    Picking the vendor directory to copy during this test since it has mor then one level of files

--- examples/directory_to_directory_copy.php
+++ examples/directory_to_directory_copy.php
@@ -7,7 +7,7 @@ require dirname(__DIR__) . '/vendor/autoload.php';
 $loop = \React\EventLoop\Factory::create();
 
 $filesystem = \React\Filesystem\Filesystem::create($loop);
-$from = $filesystem->dir(dirname(__FILE__));
+$from = $filesystem->dir(dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR . 'vendor');
 $to = $filesystem->dir(sys_get_temp_dir() . DIRECTORY_SEPARATOR . 'react_filesystem_file_to_file_copy' . DIRECTORY_SEPARATOR . uniqid());
 echo 'From: ', $from->getPath(), PHP_EOL;
 echo 'To: ', $to->getPath(), PHP_EOL;
