4fa191d141b404fc2ab61baf21f0a6bfff54056c
Cees-Jan Kiewiet <ceesjank@gmail.com>
2015-09-03 17:35:19 +0200

    Missing methods

--- tests/UnknownNodeType.php
+++ tests/UnknownNodeType.php
@@ -3,6 +3,7 @@
 namespace React\Tests\Filesystem;
 
 use React\Filesystem\Node\NodeInterface;
+use React\Filesystem\Node\ObjectStream;
 
 class UnknownNodeType implements NodeInterface
 {
@@ -36,5 +37,58 @@ class UnknownNodeType implements NodeInterface
     public function __toString()
     {
         return $this->getPath();
+    }
+
+    /**
+     * @return \React\Filesystem\AdapterInterface
+     */
+    public function getFilesystem()
+    {
+        // TODO: Implement getFilesystem() method.
+    }
+
+    /**
+     * @return \React\Promise\PromiseInterface
+     */
+    public function stat()
+    {
+        // TODO: Implement stat() method.
+    }
+
+    /**
+     * @param int $mode
+     * @return \React\Promise\PromiseInterface
+     */
+    public function chmod($mode)
+    {
+        // TODO: Implement chmod() method.
+    }
+
+    /**
+     * @param int $uid
+     * @param int $gid
+     * @return \React\Promise\PromiseInterface
+     */
+    public function chown($uid = -1, $gid = -1)
+    {
+        // TODO: Implement chown() method.
+    }
+
+    /**
+     * @param NodeInterface $node
+     * @return \React\Promise\PromiseInterface
+     */
+    public function copy(NodeInterface $node)
+    {
+        // TODO: Implement copy() method.
+    }
+
+    /**
+     * @param NodeInterface $node
+     * @return ObjectStream
+     */
+    public function copyStreaming(NodeInterface $node)
+    {
+        // TODO: Implement copyStreaming() method.
     }
 }
