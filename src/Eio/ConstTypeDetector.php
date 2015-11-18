876704e07c18ae0543af2dbde22d16523297b305
Cees-Jan Kiewiet <ceesjank@gmail.com>
2015-11-18 07:53:35 +0100

    Don't try to detect if the node doesn't hold a type

--- src/Eio/ConstTypeDetector.php
+++ src/Eio/ConstTypeDetector.php
@@ -36,13 +36,13 @@ class ConstTypeDetector implements TypeDetectorInterface
      */
     public function detect(array $node)
     {
-        if (isset($this->mapping[$node['type']])) {
-            return \React\Promise\resolve([
-                $this->filesystem,
-                $this->mapping[$node['type']],
-            ]);
+        if (!isset($node['type']) || !isset($this->mapping[$node['type']])) {
+            return new RejectedPromise();
         }
 
-        return new RejectedPromise();
+        return \React\Promise\resolve([
+            $this->filesystem,
+            $this->mapping[$node['type']],
+        ]);
     }
 }
