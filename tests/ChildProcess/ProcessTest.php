b44e1f9b44024fb7e607cb35c9ae5082c640defb
Cees-Jan Kiewiet <ceesjank@gmail.com>
2015-12-25 10:30:26 +0100

    Child process process stat test

--- tests/ChildProcess/ProcessTest.php
+++ tests/ChildProcess/ProcessTest.php
@@ -4,6 +4,7 @@ namespace React\Tests\Filesystem\ChildProcess;
 
 use React\Filesystem\ChildProcess\Process;
 use React\Tests\Filesystem\TestCase;
+use WyriHaximus\React\ChildProcess\Messenger\Messages\Payload;
 
 class ProcessTest extends TestCase
 {
@@ -20,5 +21,39 @@ class ProcessTest extends TestCase
         ;
 
         new Process($messenger);
+    }
+
+    public function testStat()
+    {
+
+        $messenger = $this->getMockBuilder('WyriHaximus\React\ChildProcess\Messenger\Messenger')
+            ->disableOriginalConstructor()
+            ->getMock()
+        ;
+
+        $resultCallbackRan = false;
+        (new Process($messenger))->stat(new Payload([
+            'path' => __FILE__,
+        ]), $messenger)->then(function ($result) use (&$resultCallbackRan) {
+            foreach ([
+                'dev',
+                'ino',
+                'mode',
+                'nlink',
+                'uid',
+                'size',
+                'gid',
+                'rdev',
+                'blksize',
+                'blocks',
+                'atime',
+                'mtime',
+                'ctime',
+            ] as $item) {
+                $this->assertTrue(isset($result[$item]));
+            }
+            $resultCallbackRan = true;
+        });
+        $this->assertTrue($resultCallbackRan);
     }
 }
