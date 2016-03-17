e526002af36659cee4e60d3c0536098075a64b8b
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-03-17 08:04:05 +0100

    File::remove test

--- tests/Adapters/FileTest.php
+++ tests/Adapters/FileTest.php
@@ -3,6 +3,7 @@
 namespace React\Tests\Filesystem\Adapters;
 
 use Clue\React\Block;
+use React\EventLoop\LoopInterface;
 use React\Filesystem\ChildProcess;
 use React\Filesystem\Eio;
 use React\Filesystem\FilesystemInterface;
@@ -13,10 +14,24 @@ class FileTest extends AbstractAdaptersTest
     /**
      * @dataProvider filesystemProvider
      */
-    public function _testStat(FilesystemInterface $filesystem)
+    public function testStat(LoopInterface $loop, FilesystemInterface $filesystem)
     {
         $actualStat = lstat(__FILE__);
-        $result = Block\await($filesystem->file(__FILE__)->stat(), $this->loop);
+        $result = Block\await($filesystem->file(__FILE__)->stat(), $loop);
         $this->assertSame($actualStat, $result);
+    }
+
+    /**
+     * @dataProvider filesystemProvider
+     */
+    public function testRemove(LoopInterface $loop, FilesystemInterface $filesystem)
+    {
+        $tempFile = $this->tmpDir . uniqid('', true);
+        touch($tempFile);
+        do {
+            sleep(1);
+        } while (!file_exists($tempFile));
+        Block\await($filesystem->file($tempFile)->remove(), $loop);
+        $this->assertFalse(file_exists($tempFile));
     }
 }
