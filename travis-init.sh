9d416ef15169db51b78338f9ca7d4d05b4dc1149
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-03-20 00:23:37 +0100

    Simplefied adding eio.so to current PHP conf

--- travis-init.sh
+++ travis-init.sh
@@ -6,7 +6,7 @@ if [[ "$TRAVIS_PHP_VERSION" != "hhvm" ]]; then
     # install 'eio' PHP extension (does not support php 7)
     if [[ "$TRAVIS_PHP_VERSION" != "7.0" ]]; then
         yes "" | pecl install eio
-        echo "extension=\"eio.so\"" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
+        echo "extension=eio.so" >> "$(php -r 'echo php_ini_loaded_file();')"
     fi
 
 fi
