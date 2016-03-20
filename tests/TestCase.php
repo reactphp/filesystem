f5eebc75f5b1b2abb57b9c4036c44b0bca891fce
Cees-Jan Kiewiet <ceesjank@gmail.com>
2016-03-20 22:14:12 +0100

    Clean up completely when test is done

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -58,6 +58,27 @@ class TestCase extends \PHPUnit_Framework_TestCase
 
     public function tearDown()
     {
-        rmdir($this->tmpDir);
+        $this->rmdir($this->tmpDir);
+    }
+
+    protected function rmdir($dir)
+    {
+        $directory = dir($dir);
+        while (false !== ($entry = $directory->read())) {
+            if (in_array($entry, ['.', '..'])) {
+                continue;
+            }
+
+            if (is_dir($dir . $entry)) {
+                $this->rmdir($dir . $entry . DIRECTORY_SEPARATOR);
+                continue;
+            }
+
+            if (is_file($dir . $entry)) {
+                unlink($dir . $entry);
+                continue;
+            }
+        }
+        $directory->close();
     }
 }
