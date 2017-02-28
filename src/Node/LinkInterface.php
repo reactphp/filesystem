fe1f166aaf0154d6c850a57feff38047891c5838
Cees-Jan Kiewiet <ceesjank@gmail.com>
2017-02-28 08:11:56 +0100

    LinkInterface documentation

--- src/Node/LinkInterface.php
+++ src/Node/LinkInterface.php
@@ -5,6 +5,8 @@ namespace React\Filesystem\Node;
 interface LinkInterface extends NodeInterface
 {
     /**
+     * Return the node this link is pointing at.
+     *
      * @return NodeInterface
      */
     public function getDestination();
