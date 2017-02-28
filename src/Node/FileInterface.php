50ed36a062d8e697acc2d2eea44981c761f8112e
Cees-Jan Kiewiet <ceesjank@gmail.com>
2017-02-28 07:53:19 +0100

    FileInterface documentation

--- src/Node/FileInterface.php
+++ src/Node/FileInterface.php
@@ -4,62 +4,86 @@ namespace React\Filesystem\Node;
 
 use React\Filesystem\AdapterInterface;
 use React\Promise\PromiseInterface;
+use React\Stream\WritableStreamInterface;
 
 interface FileInterface extends NodeInterface
 {
     /**
-     * @return \React\Promise\PromiseInterface
+     * Returns true when the file exists
+     *
+     * @return PromiseInterface
      */
     public function exists();
 
     /**
-     * @return \React\Promise\PromiseInterface
+     * Remove the file
+     *
+     * @return PromiseInterface
      */
     public function remove();
 
     /**
+     * Open the file and return a promise resolve into a stream that can be read from or written to.
+     *
      * @param $flags
      * @param string $mode
-     * @return mixed
+     * @return PromiseInterface<GenericStreamInterface>
      */
     public function open($flags, $mode = AdapterInterface::CREATION_MODE);
 
     /**
-     * @return \React\Promise\PromiseInterface
+     * Return the change time, access time, and modification time
+     *
+     * @return PromiseInterface
      */
     public function time();
 
     /**
+     * Rename the file and return the new file through a promise
+     *
      * @param string $toFilename
-     * @return \React\Promise\PromiseInterface
+     * @return PromiseInterface<FileInterface>
      */
     public function rename($toFilename);
 
     /**
-     * @return \React\Promise\PromiseInterface
+     * Return the size of the file.
+     *
+     * @return PromiseInterface
      */
     public function size();
 
     /**
+     * Create the file
+     *
      * @param string $mode
      * @param null $time
-     * @return \React\Promise\PromiseInterface
+     * @return PromiseInterface
+     *
+     * @throws \Exception When the file already exists
      */
     public function create($mode = AdapterInterface::CREATION_MODE, $time = null);
 
     /**
+     * Touch the file, modifying it's mtime when it exists,
+     * or creating the file when it doesn't it exists.
+     *
      * @param string $mode
      * @param null $time
-     * @return \React\Promise\PromiseInterface
+     * @return PromiseInterface
      */
     public function touch($mode = AdapterInterface::CREATION_MODE, $time = null);
 
     /**
-     * @return PromiseInterface
+     * Open the file and read all its contents returning those through a promise.
+     *
+     * @return PromiseInterface<string>
      */
     public function getContents();
 
     /**
+     * Write the given contents to the file, overwriting any existing contents or creating the file.
+     *
      * @param string $contents
      * @return PromiseInterface
      */
