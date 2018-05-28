50391bc92e8874486d1f58a845e239805747c753
Cees-Jan Kiewiet <ceesjank@gmail.com>
2018-05-28 18:33:08 +0200

    EIO supports PHP 7 these days, but breaks on nightly

--- travis-init.sh
+++ travis-init.sh
@@ -2,11 +2,9 @@
 
 if [[ "$TRAVIS_PHP_VERSION" != "hhvm" ]]; then
     # install 'eio' PHP extension (does not support php 7)
-    if [[ "$TRAVIS_PHP_VERSION" != "7.0" ]]; then
+    if [[ "$TRAVIS_PHP_VERSION" != "nightly" ]]; then
         yes "" | pecl install eio
-        echo "extension=eio.so" >> "$(php -r 'echo php_ini_loaded_file();')"
     fi
-
 fi
 
 #set -e
